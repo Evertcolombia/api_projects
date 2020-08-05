51519e6041e73c8aa64724d1cb7703b03740d6e0
evertcolombia <evertescalante@gmail.com>
2020-08-04 20:38:23 -0500

    adding user model and user model manager, also create migration file and make migrations

--- profiles_rest_api/profiles_project/settings.py
+++ profiles_rest_api/profiles_project/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# use this model as default
+# user model instead of the ones provided for django
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
