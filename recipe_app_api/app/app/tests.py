1e601d73213c04e043c156d3d681a1b9d3fae567
evertcolombia <evertescalante@gmail.com>
2020-08-11 17:11:55 -0500

    hotfix

--- recipe_app_api/app/app/tests.py
+++ recipe_app_api/app/app/tests.py
@@ -6,8 +6,8 @@ class CalcTests(TestCase):
     
     def test_add_numbers(self):
         """Test that two number are added together"""
-        self.assertEqual(add(3, 8), 11)
+        self.assertEqual(add(2, 8), 10)
 
     def test_subtract_number(self):
         """Test that values are subtracted and returned"""
-        self.assertEqual(subtract(11, 5), 6)
+        self.assertEqual(subtract(11, 4), 7)
